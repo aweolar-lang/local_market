ae4cc52bf8d49d13ae408765cd046811f181ba36
Denis Mutugi <aweolar@gmail.com>
2026-03-25 22:06:38 +0300

    good

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center py-12 px-4 sm:px-6 lg:px-8 bg-gray-200 relative overflow-hidden">
+    <div className="min-h-screen flex flex-col justify-center items-center py-12 px-4 sm:px-6 lg:px-8 bg-gray-200 relative overflow-hidden border-radius: 0.125rem;">
       
       <div className="absolute top-[-10%] left-[-10%] w-[50%] h-[50%] bg-green-400/10 blur-[120px] rounded-full pointer-events-none"></div>
       <div className="absolute bottom-[-10%] right-[-10%] w-[50%] h-[50%] bg-emerald-400/5 blur-[120px] rounded-full pointer-events-none"></div>
